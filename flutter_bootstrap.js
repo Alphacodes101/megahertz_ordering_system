dc8bb419462868cdaca5d90205ac866d8ce90d2d
rehyan_yadav <ritulyadav1984@gmail.com>
2024-12-24 00:55:40 +0530

    testing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4130884248"
+    serviceWorkerVersion: "3021968227"
   }
 });
